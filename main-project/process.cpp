9063b301ab1867063af5236952d43a95cbef30fc
Prombl7 <ivanburunov2007@gmail.com>
2026-05-21 17:51:24 +0300

    Реализована функция findMaxDurationIndex

--- main-project/process.cpp
+++ main-project/process.cpp
@@ -1,5 +1,18 @@
 #include "processing.h"
 
 int findMaxDurationIndex(Report reports[], int n) {
-    return 0;
+    if (n == 0) return -1;
+    int maxIndex = 0;
+    int maxDur = (reports[0].endHour * 60 + reports[0].endMin)
+        - (reports[0].startHour * 60 + reports[0].startMin);
+
+    for (int i = 1; i < n; i++) {
+        int dur = (reports[i].endHour * 60 + reports[i].endMin)
+            - (reports[i].startHour * 60 + reports[i].startMin);
+        if (dur > maxDur) {
+            maxDur = dur;
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
 }
