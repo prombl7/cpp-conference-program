ab94d8177aa49b9defcc1c75c76cf3784fdcba61
Prombl7 <ivanburunov2007@gmail.com>
2026-05-21 17:29:51 +0300

    Добавлены модульные тесты (падают)

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../main-project/conference.h"
+#include "../main-project/processing.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +10,48 @@ namespace unittestproject
     TEST_CLASS(unittestproject)
     {
     public:
-        TEST_METHOD(TestMethod1)
+
+        // 1 один доклад индекс максимального  0
+        TEST_METHOD(Test_OneReport_Index0)
         {
-            Assert::Fail();
+            Report reports[1];
+            reports[0].startHour = 9;  reports[0].startMin = 0;
+            reports[0].endHour = 9;    reports[0].endMin = 30;
+            Assert::AreEqual(0, findMaxDurationIndex(reports, 1));
+        }
+
+        // 2три доклада  самый длинный второй индекс 1
+        TEST_METHOD(Test_ThreeReports_Index1)
+        {
+            Report reports[3];
+            
+            reports[0].startHour = 9;  reports[0].startMin = 0;
+            reports[0].endHour = 9;    reports[0].endMin = 20;
+           
+            reports[1].startHour = 10; reports[1].startMin = 0;
+            reports[1].endHour = 10;   reports[1].endMin = 50;
+           
+            reports[2].startHour = 11; reports[2].startMin = 0;
+            reports[2].endHour = 11;   reports[2].endMin = 30;
+
+            Assert::AreEqual(1, findMaxDurationIndex(reports, 3));
+        }
+
+        // 3 самый длинный последний индекс 2
+        TEST_METHOD(Test_ThreeReports_Index2)
+        {
+            Report reports[3];
+            
+            reports[0].startHour = 9;  reports[0].startMin = 0;
+            reports[0].endHour = 9;    reports[0].endMin = 10;
+           
+            reports[1].startHour = 10; reports[1].startMin = 0;
+            reports[1].endHour = 10;   reports[1].endMin = 15;
+           
+            reports[2].startHour = 11; reports[2].startMin = 0;
+            reports[2].endHour = 12;   reports[2].endMin = 0;
+
+            Assert::AreEqual(2, findMaxDurationIndex(reports, 3));
         }
     };
 }
